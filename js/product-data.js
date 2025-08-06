cd7b4e31ee07d85e0edf1aae1f417c77a46b7be0
AmeyGarkar <ameygarkar1@gmail.com>
2025-08-06 19:07:01 +0530

    Product page theme added

--- js/product-data.js
+++ js/product-data.js
@@ -54,5 +54,34 @@ window.productsData = {
       "Microphone": "Dual Mic with ENC",
       "Color": "Matte Purple"
     }
-  }
+  },
+
+  "product3": {
+    name: "Amazfit Bip 3 pro",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+    images: [
+      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image1.jpg",
+      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image2.jpg",
+      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image3.jpg"
+
+    ],
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
 };
