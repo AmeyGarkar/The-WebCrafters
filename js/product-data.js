6dba0424a9d4553cc8808d062bef5a596fb03e5a
ishwari1201 <ishwari.1201@gmail.com>
2025-08-06 17:48:09 +0530

    adding img

--- js/product-data.js
+++ js/product-data.js
@@ -26,5 +26,33 @@ window.productsData = {
       "Microphone": "Dual Mic with ENC",
       "Color": "Matte Purple"
     }
+  },
+
+   "product2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+    images: [
+".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image1.jpg",
+".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image1.webp",
+".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image2.jpg"
+    ],
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
   }
 };
