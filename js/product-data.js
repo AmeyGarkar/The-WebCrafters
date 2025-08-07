b98e946f0f040dd613e56047e7d350d5c820fc54
ishwari1201 <ishwari.1201@gmail.com>
2025-08-07 16:31:56 +0530

    modiefy index.html

--- js/product-data.js
+++ js/product-data.js
@@ -1,5 +1,5 @@
 window.productsData = {
-  "product1": {
+  "boAt_Airdopes141": {
     name: "Air Clips - Wireless Earbuds",
     description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
     price: 2526,
@@ -28,18 +28,21 @@ window.productsData = {
     }
   },
 
-   "product2": {
+   "Noise_AirClipsWirelessAirbuds": {
     name: "Noise_AirClipsWirelessAirbuds",
     description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
     price: 2526,
     oldPrice: 3999,
     stock: 14,
     rating: "4.9, 421 ratings",
-    images: [
-".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image1.jpg",
-".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image1.webp",
-".\The-WebCrafters\assets\images\Earbuds\Noise_AirClipsWirelessAirbuds\image2.jpg"
-    ],
+   
+images: [
+  "../assets/images/Earbuds/Noise_AirClipsWirelessAirbuds/image1.jpg",
+  "../assets/images/Earbuds/Noise_AirClipsWirelessAirbuds/image1.webp",
+  "../assets/images/Earbuds/Noise_AirClipsWirelessAirbuds/image2.jpgg"
+],
+
+  
     highlights: [
       "Airwave™ Technology with Deep Bass",
       "Upto 45 Hours Total Playback",
@@ -54,5 +57,228 @@ window.productsData = {
       "Microphone": "Dual Mic with ENC",
       "Color": "Matte Purple"
     }
-  }
+  },
+    "Oneplus_NordBuds2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Earbuds/Oneplus_NordBuds2/image1.webp",
+  "../assets/images/Earbuds/Oneplus_NordBuds2/image2.jpg",
+  "../assets/images/Earbuds/Oneplus_NordBuds2/image3.jpg",
+ " ../assets/images/Earbuds/Oneplus_NordBuds2/image4.jpg"
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+  "Realme_WirelessEarbudsT300": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Earbuds/Realme_WirelessEarbudsT300/image1.jpg",
+
+  "../assets/images/Earbuds/Realme_WirelessEarbudsT300/image3.jpg",
+  "../assets/images/Earbuds/Realme_WirelessEarbudsT300/image4.jpg",
+ " ../assets/images/Earbuds/Realme_WirelessEarbudsT300/image5.jpg"
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+   "Samsung_GalaxyBuds2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Earbuds/Samsung_GalaxyBuds2/image1.jpg",
+  "../assets/images/Earbuds/Samsung_GalaxyBuds2/image2.jpg",
+  "../assets/images/Earbuds/Samsung_GalaxyBuds2/image3.jpg"
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+   "boAt Rockerz 450": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Headphones/boAt Rockerz 450/image1.jpg",
+
+  "../assets/images/Headphones/boAt Rockerz 450/image3.jpg",
+  "../assets/images/Headphones/boAt Rockerz 450/image4.jpg",
+ " ../assets/images/Headphones/boAt Rockerz 450/image5.jpg"
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+   "JBL Tune 510BT": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Headphones/JBL Tune 510BT/image1.jpg",
+
+  "../assets/images/Headphones/JBL Tune 510BT/image4.jpg",
+  "../assets/images/Headphones/JBL Tune 510BT/image5.jpg"
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+  "Skullcandy Riff Wireless 2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Headphones/Skullcandy Riff Wireless 2/image1.jpg",
+
+  "../assets/images/Headphones/Skullcandy Riff Wireless 2/image2.jpg",
+  "../assets/images/Headphones/Skullcandy Riff Wireless 2/image3.jpg",
+"../assets/images/Headphones/Skullcandy Riff Wireless 2/image5.jpg"
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+   "Sony WH-CH520": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Headphones/Sony WH-CH520/image1.jpg",
+
+  "../assets/images/Headphones/Sony WH-CH520/image2.jpg",
+  "../assets/images/Headphones/Sony WH-CH520/image5.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+   
 };
