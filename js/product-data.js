b46986d07526ee313dd18053bd435ae940626136
ishwari1201 <ishwari.1201@gmail.com>
2025-08-07 19:39:20 +0530

    all images added

--- js/product-data.js
+++ js/product-data.js
@@ -58,20 +58,13 @@ images: [
       "Color": "Matte Purple"
     }
   },
-<<<<<<< HEAD
     "Oneplus_NordBuds2": {
     name: "Noise_AirClipsWirelessAirbuds",
-=======
-
-  "product3": {
-    name: "Amazfit Bip 3 pro",
->>>>>>> 48718e88fb0ec11047a5056b83384c1cdb6c8b6c
     description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
     price: 2526,
     oldPrice: 3999,
     stock: 14,
     rating: "4.9, 421 ratings",
-<<<<<<< HEAD
    
 images: [
   "../assets/images/Earbuds/Oneplus_NordBuds2/image1.webp",
@@ -81,14 +74,6 @@ images: [
 ],
 
   
-=======
-    images: [
-      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image1.jpg",
-      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image2.jpg",
-      " ../assets/images/Smartwatches/Amazfit Bip 3 Pro/image3.jpg"
-
-    ],
->>>>>>> 48718e88fb0ec11047a5056b83384c1cdb6c8b6c
     highlights: [
       "Airwave™ Technology with Deep Bass",
       "Upto 45 Hours Total Playback",
@@ -104,7 +89,6 @@ images: [
       "Color": "Matte Purple"
     }
   },
-<<<<<<< HEAD
   "Realme_WirelessEarbudsT300": {
     name: "Noise_AirClipsWirelessAirbuds",
     description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
@@ -296,7 +280,538 @@ images: [
       "Color": "Matte Purple"
     }
   },
+   "Zebronics Zeb-Rush": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
    
-=======
->>>>>>> 48718e88fb0ec11047a5056b83384c1cdb6c8b6c
+images: [
+  "../assets/images/Headphones/Zebronics Zeb-Rush/image1.jpg",
+
+  "../assets/images/Headphones/Zebronics Zeb-Rush/image2.jpg",
+  "../assets/images/Headphone/Zebronics Zeb-Rush/image2.jpg"
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"boAt Rockerz 255 Pro+": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Neckbands/boAt Rockerz 255 Pro+/image1.jpg",
+
+  "../assets/images/Neckbands/boAt Rockerz 255 Pro+/image2.jpg",
+  "../assets/images/Neckbands/boAt Rockerz 255 Pro+/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Boult Audio ProBass Curve ANC": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Neckbands/Boult Audio ProBass Curve ANC/image1.jpg",
+
+  "../assets/images/Neckbands/Boult Audio ProBass Curve ANC/image2.jpg",
+  "../assets/images/Neckbands/Boult Audio ProBass Curve ANC/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
+     "Boat_WaveCall2Plus": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Smartwatches/Boat_WaveCall2Plus/image1.jpg",
+
+  "../assets/images/Smartwatches/Boat_WaveCall2Plus/image2.jpg",
+  "../assets/images/Smartwatches/Boat_WaveCall2Plus/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"realme Buds Wireless 2 Neo": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Neckbands/realme Buds Wireless 2 Neo/image1.jpg",
+
+  "../assets/images/Neckbands/realme Buds Wireless 2 Neo/image2.jpg",
+  "../assets/images/Neckbands/realme Buds Wireless 2 Neo/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Sony WI-C100 Wireless Neckband": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Neckbands/Sony WI-C100 Wireless Neckband/image1.jpg",
+
+  "../assets/images/Neckbands/Sony WI-C100 Wireless Neckband/image2.jpg",
+  "../assets/images/Neckbands/Sony WI-C100 Wireless Neckband/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Wings Phantom 315 Neckband": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Neckbands/Wings Phantom 315 Neckband/image1.jpg",
+
+  "../assets/images/Neckbands/Wings Phantom 315 Neckband/image3.jpg",
+  "../assets/images/Neckbands/Wings Phantom 315 Neckband/image4.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Sony WH-CH520": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Headphones/Sony WH-CH520/image1.jpg",
+
+  "../assets/images/Headphones/Sony WH-CH520/image2.jpg",
+  "../assets/images/Headphones/Sony WH-CH520/image5.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Amazfit Bip 3 Pro": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Smartwatches/Amazfit Bip 3 Pro/image1.jpg",
+
+  "../assets/images/Smartwatches/Amazfit Bip 3 Pro/image2.jpg",
+  "../assets/images/Smartwatches/Amazfit Bip 3 Pro/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+   
+
+  },"Fire-Boltt Phoenix Pro": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Smartwatches/Fire-Boltt Phoenix Pro/image1.jpg",
+
+  "../assets/images/Smartwatches/Fire-Boltt Phoenix Pro/image2.jpg",
+  "../assets/images/Smartwatches/Fire-Boltt Phoenix Pro/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Noise_ColorFitIcon2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Smartwatches/Noise_ColorFitIcon2/image1.webp",
+
+  "../assets/images/Smartwatches/Noise_ColorFitIcon2/image2.webp",
+  "../assets/images/Smartwatches/Noise_ColorFitIcon2/image3.webp "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"realme Watch 3 Pro": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Smartwatches/realme Watch 3 Pro/image1.jpg",
+
+  "../assets/images/Smartwatches/realme Watch 3 Pro/image2.jpg",
+  "../assets/images/Smartwatches/realme Watch 3 Pro/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"boAt Stone 1200F": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Speakers/boAt Stone 1200F/image1.jpg",
+
+  "../assets/images/Speakers/boAt Stone 1200F/image2.jpg",
+  "../assets/images/Speakers/boAt Stone 1200F/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"JBL Go 3": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Speakers/JBL Go 3/image1.jpg",
+
+  "../assets/images/Speakers/JBL Go 3/image2.jpg",
+  "../assets/images/Speakers/JBL Go 3/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Mivi Roam 2": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Speakers/Mivi Roam 2/image1.jpg",
+
+  "../assets/images/Speakers/Mivi Roam 2/image2.jpg",
+  "../assets/images/Speakers/Mivi Roam 2/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },"Sony SRS-XB13": {
+    name: "Noise_AirClipsWirelessAirbuds",
+    description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+    price: 2526,
+    oldPrice: 3999,
+    stock: 14,
+    rating: "4.9, 421 ratings",
+   
+images: [
+  "../assets/images/Speakers/Sony SRS-XB13/image1.jpg",
+
+  "../assets/images/Speakers/Sony SRS-XB13/image2.jpg",
+  "../assets/images/Speakers/Sony SRS-XB13/image3.jpg "
+
+],
+
+  
+    highlights: [
+      "Airwave™ Technology with Deep Bass",
+      "Upto 45 Hours Total Playback",
+      "Instant Wake-N-Pair Technology",
+      "Low Latency for Gaming",
+      "USB-C Fast Charging"
+    ],
+    info: {
+      "Battery": "450mAh Charging Case",
+      "Warranty": "1 Year Brand Warranty",
+      "Compatibility": "Android & iOS",
+      "Microphone": "Dual Mic with ENC",
+      "Color": "Matte Purple"
+    }
+  },
 };
+//   },"Sony WH-CH520": {
+//     name: "Noise_AirClipsWirelessAirbuds",
+//     description: "Experience the next-level OWS with Airwave™ Technology for immersive audio. 45H battery backup, sweat resistant.",
+//     price: 2526,
+//     oldPrice: 3999,
+//     stock: 14,
+//     rating: "4.9, 421 ratings",
+   
+// images: [
+//   "../assets/images/Speakers/Zebronics Zeb-Vita Pro/image1.jpg",
+
+//   "../assets/images/Speakers/Zebronics Zeb-Vita Pro/image2.jpg",
+//   "../assets/images/Speakers/Zebronics Zeb-Vita Pro/image3.jpg "
+
+// ],
+
+  
+//     highlights: [
+//       "Airwave™ Technology with Deep Bass",
+//       "Upto 45 Hours Total Playback",
+//       "Instant Wake-N-Pair Technology",
+//       "Low Latency for Gaming",
+//       "USB-C Fast Charging"
+//     ],
+//     info: {
+//       "Battery": "450mAh Charging Case",
+//       "Warranty": "1 Year Brand Warranty",
+//       "Compatibility": "Android & iOS",
+//       "Microphone": "Dual Mic with ENC",
+//       "Color": "Matte Purple"
+//     }
+//   },
+// };
+  
